92870da8a6d85899268f8b80c8c973c9c728d454
TongZhangzt <toz7@pitt.edu>
2017-02-24 11:41:14 -0500

    Add support for IOS platform

--- src/ios/NativeRingtones.m
+++ src/ios/NativeRingtones.m
@@ -1,7 +1,12 @@
 /********* NativeRingtones.m Cordova Plugin Implementation *******/
 
+#import "NativeRingtones.h"
 #import <Cordova/CDV.h>
-#import "APPLocalNotificationOptions.h"
+#import <UIKit/UIKit.h>
+#import <AudioToolbox/AudioToolbox.h>
+#import <Foundation/Foundation.h>
+
+NSString* const DEFAULT_SOUND = @"res://platform_default";
 
 @interface NativeRingtones : CDVPlugin {
   // Member variables go here.
@@ -14,14 +19,53 @@
 
 - (void)get:(CDVInvokedUrlCommand*)command
 {
-
-
     CDVPluginResult* pluginResult = nil;
-    NSString* echo = [command.arguments objectAtIndex:0];
+    //NSString* echo = [command.arguments objectAtIndex:0];
 
+    NSMutableArray *audioFileList;
+    NSMutableArray *_systemSounds = nil;
 
-    if (echo != nil && [echo length] > 0) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:UILocalNotificationDefaultSoundName];
+    audioFileList = [[NSMutableArray alloc] init];
+    _systemSounds = [NSMutableArray array];
+
+    NSFileManager *fileManager = [[NSFileManager alloc] init];
+    NSURL *directoryURL = [NSURL URLWithString:@"/System/Library/Audio/UISounds"];
+    NSArray *keys = [NSArray arrayWithObject:NSURLIsDirectoryKey];
+
+    NSDirectoryEnumerator *enumerator = [fileManager
+                                         enumeratorAtURL:directoryURL
+                                         includingPropertiesForKeys:keys
+                                         options:0
+                                         errorHandler:^(NSURL *url, NSError *error) {
+                                             // Handle the error.
+                                             // Return YES if the enumeration should continue after the error.
+                                             return YES;
+                                         }];
+
+    for (NSURL *url in enumerator) {
+        NSError *error;
+        NSNumber *isDirectory = nil;
+        if (! [url getResourceValue:&isDirectory forKey:NSURLIsDirectoryKey error:&error]) {
+            // handle error
+        }
+        else if (! [isDirectory boolValue]) {
+            NSString *myString = url.absoluteString;
+            [audioFileList addObject:[myString pathComponents].lastObject];
+
+            SystemSoundID soundID;
+            AudioServicesCreateSystemSoundID((__bridge_retained CFURLRef)url, &soundID);
+
+            SoundInfomation *sound = [[SoundInfomation alloc] init];
+            sound.soundID   = soundID;
+            sound.soundUrl  = url;
+            sound.soundName = url.lastPathComponent;
+
+            [_systemSounds addObject:sound];
+        }
+    }
+
+    if (DEFAULT_SOUND != nil) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsArray:_systemSounds];
     } else {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
     }
