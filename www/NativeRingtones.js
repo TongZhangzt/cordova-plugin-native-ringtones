c45d5062d0cebc2a34d011255a67a220f84a6a27
TongZhangzt <toz7@pitt.edu>
2017-04-12 23:45:27 -0400

    Modify for custom ringtones

--- www/NativeRingtones.js
+++ www/NativeRingtones.js
@@ -11,15 +11,24 @@ function RingtoneManager() {
  * @param {Object}
  *            Set the ringtone list to the attribute ringtoneList of the object
  */
-RingtoneManager.prototype.getRingtone = function(successCallback, errorCallback, ringtoneType) {
+RingtoneManager.prototype.getRingtone = function (successCallback, errorCallback, ringtoneType) {
     exec(successCallback, errorCallback, "NativeRingtones", "get", [ringtoneType]);
 };
 
-RingtoneManager.prototype.playRingtone = function(successCallback, errorCallback, ringtoneUri) {
-    exec(successCallback, errorCallback, "NativeRingtones", "play", [ringtoneUri]);
+RingtoneManager.prototype.playRingtone = function (successCallback, errorCallback, ringtoneUri) {
+    if (ringtoneUri.indexOf("content") >= 0) {
+        exec(successCallback, errorCallback, "NativeRingtones", "play", [ringtoneUri]);
+    }
+    else {
+        var contentPath = window.location.pathname.substr(window.location.pathname, window.location.pathname.length - 10);
+        var path = "file://" + contentPath + ringtoneUri.substr(ringtoneUri, ringtoneUri - 7);
+        new Media(path, function (success) {
+            console.log(success);
+        }).play();
+    }
 };
 
-RingtoneManager.prototype.stopRingtone = function(successCallback, errorCallback, ringtoneUri) {
+RingtoneManager.prototype.stopRingtone = function (successCallback, errorCallback, ringtoneUri) {
     exec(successCallback, errorCallback, "NativeRingtones", "stop", [ringtoneUri]);
 };
 
